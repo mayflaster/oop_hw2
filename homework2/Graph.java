4e2613b1874ee1023a6b77d2075cf868c7020e4a
mayflaster <mayflaster96@gmail.com>
2020-12-21 20:10:58 +0200

    Initial commit

---
+++ homework2/Graph.java
@@ -0,0 +1,25 @@
+package homework2;
+
+public class Graph<E> {
+
+
+    /**
+     * CreateGraph graphName
+     */
+    public Graph(){
+        //TODO
+    }
+
+    /**
+     * CreateNode nodeName cost
+     */
+    public void addNode(E node){
+        //TODO
+    }
+
+    public void addEdge (E parentNote, E childNode){
+
+    }
+
+
+}
